314c4a423d57f12b90667eb9b51c142baa6dbf47
RISHIRAJ <58257428+rishiraj1977@users.noreply.github.com>
2020-12-15 10:07:52 +0530

    updated the code

--- 121.py
+++ 121.py
@@ -1,36 +1,31 @@
-import cv2 
-import time
+import cv2
 import numpy as np
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-output_file = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+
 cap = cv2.VideoCapture(0)
-time.sleep(2)
-bg = 0
-for i in range (60):
-    ret,bg = cap.read()
-bg = np.flip(bg,axis = 1)
-while(cap.isOpened()):
-    ret,img = cap.read()
-    if not ret:
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
         break
-    img = np.flip(img,axis = 1)
-    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)  
-    lowerred = np.array([0,120,50])
-    upperred = np.array([10,255,255])
-    mask1 = cv2.inRange(hsv,lowerred,upperred)
-    lowerred = np.array([170,120,70])
-    upperred = np.array([180,255,255])
-    mask2 = cv2.inRange(hsv,lowerred,upperred)
-    mask1 = mask1+mask2
-    mask1 = cv2.morphologyEx(mask1,cv2.MORPH_OPEN,np.ones((3,3),np.uint8))
-    mask1 = cv2.morphologyEx(mask1,cv2.MORPH_DILATE,np.ones((3,3),np.uint8))
-    mask2 = cv2.bitwise_not(mask1)
-    res1 = cv2.bitwise_and(img,img,mask = mask2)
-    res2 = cv2.bitwise_and(bg,bg,mask = mask1)
-    finaloutput = cv2.addWeighted(res1,1,res2,1,0)
-    output_file.write(finaloutput)
-    cv2.imshow('magic',finaloutput)
-    cv2.waitKey(1)
-cap.release()
-out.release()
+
 cv2.destroyAllWindows()
